275807e0831c23a20f2984fb34ceeff1be794755
cerelus <cerelus3@gmail.com>
2023-03-12 09:17:38 +0900

    0312

---
+++ 03.lab/01.slide/js/bslide.js
@@ -0,0 +1,17 @@
+// btn_slide.html - 메인JS
+
+// 캐릭터 데이터 셋팅
+const cat = {
+    red: {name: "Lightning McQueen"
+    description
+    ,
+    "Number 95 Lightning McQueen is still a hotshot race car, but his rookie days are past him. Now a permanent resident of Radiator Springs, Lightning has become a bona fide worldwide celebrity as winner of four Piston Cups."
+};
+
+// 로드구역
+window.addEventListener("DOMContentLoaded",loadFn);
+
+// 로드함수
+function loadFn(){
+
+}
